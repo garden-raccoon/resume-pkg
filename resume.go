5bfd1e42acfec718845703a3139758414c56560f
garden-raccoon <grachevs379@gmail.com>
2024-12-12 12:52:03 +0100

    healthcheck

--- resume.go
+++ resume.go
@@ -2,7 +2,11 @@ package resume
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/health/grpc_health_v1"
+	"sync"
 	"time"
 
 	"google.golang.org/grpc"
@@ -20,6 +24,9 @@ type IResumeAPI interface {
 	CreateResume(res *models.Resume) error
 
 	GetResumes() ([]*models.Resume, error)
+
+	HealthCheck() error
+
 	// Close GRPC Api connection
 	Close() error
 }
@@ -29,8 +36,10 @@ type IResumeAPI interface {
 type Api struct {
 	addr    string
 	timeout time.Duration
+	mu      sync.Mutex
 	*grpc.ClientConn
 	proto.ResumeServiceClient
+	grpc_health_v1.HealthClient
 }
 
 // New create new Battles Api instance
@@ -38,8 +47,9 @@ func New(addr string) (IResumeAPI, error) {
 	api := &Api{timeout: timeOut * time.Second}
 
 	if err := api.initConn(addr); err != nil {
-		return nil, fmt.Errorf("create Battles Api:  %w", err)
+		return nil, fmt.Errorf("create ResumeApi:  %w", err)
 	}
+	api.HealthClient = grpc_health_v1.NewHealthClient(api.ClientConn)
 
 	api.ResumeServiceClient = proto.NewResumeServiceClient(api.ClientConn)
 	return api, nil
@@ -64,7 +74,10 @@ func (api *Api) initConn(addr string) (err error) {
 		PermitWithoutStream: true,             // send pings even without active streams
 	}
 
-	api.ClientConn, err = grpc.Dial(addr, grpc.WithInsecure(), grpc.WithKeepaliveParams(kacp))
+	api.ClientConn, err = grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithKeepaliveParams(kacp))
+	if err != nil {
+		return fmt.Errorf("failed to dial: %w", err)
+	}
 	return
 }
 func (api *Api) GetResumes() ([]*models.Resume, error) {
@@ -81,4 +94,23 @@ func (api *Api) GetResumes() ([]*models.Resume, error) {
 	resumes := models.ResumesFromProto(resp)
 
 	return resumes, nil
+}
+func (api *Api) HealthCheck() error {
+	ctx, cancel := context.WithTimeout(context.Background(), api.timeout)
+	defer cancel()
+
+	api.mu.Lock()
+	defer api.mu.Unlock()
+
+	resp, err := api.HealthClient.Check(ctx, &grpc_health_v1.HealthCheckRequest{Service: "userapi"})
+	if err != nil {
+		return fmt.Errorf("healthcheck error: %w", err)
+	}
+
+	if resp.Status != grpc_health_v1.HealthCheckResponse_SERVING {
+		return fmt.Errorf("node is %s", errors.New("service is unhealthy"))
+	}
+	fmt.Println("HEALTHY")
+	//api.status = service.StatusHealthy
+	return nil
 }
